cf6051aa0852e2cfb36686dce43480eb52ae3366
aidarous <aidarous12@gmail.com>
2021-11-04 19:01:29 -0400

    static search bar and button created

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
